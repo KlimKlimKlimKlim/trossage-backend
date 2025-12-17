50f0884530309864f3229863c6098f7d79d05399
Egor <egor@6693790.ru>
2025-12-17 21:15:42 +0300

    Moved dto.

--- internal/http/servers/api/register_user.go
+++ internal/http/servers/api/register_user.go
@@ -3,8 +3,8 @@ package api
 import (
 	"github.com/gin-gonic/gin"
 
-	"github.com/KlimKlimKlimKlim/trossage-backend/internal/dto"
 	derrors "github.com/KlimKlimKlimKlim/trossage-backend/internal/errors"
+	"github.com/KlimKlimKlimKlim/trossage-backend/internal/http/dto"
 	"github.com/KlimKlimKlimKlim/trossage-backend/internal/http/response"
 )
 
