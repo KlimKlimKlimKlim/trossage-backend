9b52e8ab00186e77e6a4ef77ceaa239285ffb898
Egor <egor@6693790.ru>
2025-12-20 16:25:20 +0300

    Added squawk and refactored .sql files.

--- migrations/000001_init_users_table.down.sql
+++ migrations/000001_init_users_table.down.sql
@@ -1,3 +1,8 @@
+-- squawk-ignore-file ban-drop-table, require-concurrent-index-deletion
+SET lock_timeout = '2s';
+
+SET statement_timeout = '5s';
+
 DROP TRIGGER IF EXISTS set_updated_at_users ON users;
 
 DROP FUNCTION IF EXISTS update_updated_at_users ();
--- migrations/000002_init_refresh_tokens_table.down.sql
+++ migrations/000002_init_refresh_tokens_table.down.sql
@@ -1,3 +1,8 @@
+-- squawk-ignore-file ban-drop-table, require-concurrent-index-deletion
+SET lock_timeout = '2s';
+
+SET statement_timeout = '5s';
+
 DROP INDEX IF EXISTS idx_refresh_tokens_revoked;
 
 DROP INDEX IF EXISTS idx_refresh_tokens_expired;
